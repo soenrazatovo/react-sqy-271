d363724ed8626624583fdf2d02431a0ccda49c50
Soen Razatovo <soen.razatovo@gmail.com>
2026-03-24 16:28:19 +0100

    :sparkles: Todo List PUT/DELETE added

--- src/Todo.jsx
+++ src/Todo.jsx
@@ -5,6 +5,8 @@ function Todo() {
 
     const [todos, setTodos] = useState([])
     const [newTodoContent, setNewTodoContent] = useState("")
+    const [idEditTodo, setIdEditTodo] = useState()
+    const [contentEditTodo, setContentEditTodo] = useState()
 
     async function fetchTodos() {
         const res = await fetch("http://localhost:3000/todo", {
@@ -19,7 +21,7 @@ function Todo() {
         setTodos(data)
     }
 
-    const handleSubmit = async (e) => {
+    async function handleSubmit(e){
         e.preventDefault()
 
         if (newTodoContent != "") {
@@ -38,10 +40,62 @@ function Todo() {
 
             setNewTodoContent("")
             fetchTodos()
+
+        }
+    }
+
+    async function handleDelete(id) {
+        const res = await fetch("http://localhost:3000/todo/delete", {
+            method: "DELETE",
+            body : JSON.stringify({id}),
+            headers: {
+                "Accept" : "application/json",
+                "Content-type" : "application/json",
+            }
+        })
+
+        const data = await res.json()
+        alert(data)
+        fetchTodos()
+    }
+
+    async function handleCheckboxChange(e, todo) {
+
+        const res = await fetch("http://localhost:3000/todo/modify", {
+            method: "PUT",
+            body : JSON.stringify({id: todo.id, content: todo.content, checked: e.target.checked}),
+            headers: {
+                "Accept" : "application/json",
+                "Content-type" : "application/json",
+            }
+        })
+
+        const data = await res.json()
+        alert(data)
+        fetchTodos()
+    }
+
+    async function handleContentChange(todo) {
+
+        if (todo.content != contentEditTodo){
+            const res = await fetch("http://localhost:3000/todo/modify", {
+                method: "PUT",
+                body : JSON.stringify({id: todo.id, content: contentEditTodo, checked: todo.checked}),
+                headers: {
+                    "Accept" : "application/json",
+                    "Content-type" : "application/json",
+                }
+            })
+            const data = await res.json()
+            alert(data)
+            fetchTodos()
         }
 
-
+        setIdEditTodo()
     }
+
+    
+
     useEffect(()=>{
         fetchTodos()
     },[])
@@ -49,21 +103,37 @@ function Todo() {
     return ( 
         <>
             <h1>ToDo List : </h1>
-            
-            <form onSubmit={handleSubmit} style={{display: "flex", gap: "8px"}}>
+
+            <form onSubmit={(e)=>{handleSubmit(e)}} style={{display: "flex", gap: "8px"}}>
                 <input type="text" onChange={(e)=>{setNewTodoContent(e.target.value)}} value={newTodoContent} placeholder="Enter your task ..." required/>
                 <input type="submit"/>
             </form>
 
             <div style={{display: "flex", flexDirection: "column", gap: "32px", margin: "32px"}}>
-                {todos && todos.map(todo => (
-                    <div key={todo.id} style={{display: "flex", justifyContent: "center", alignItems: "center", gap: "16px", width: "fit-content", margin: "auto", backgroundColor: "grey", padding: "16px", borderRadius: "32px"}}>
-                        <input type="checkbox" checked={todo.checked} style={{display: "flex", justifyContent: "center", alignItems: "center", width: "fit-content", margin: "0px", boxSizing: ""}}/>
-                        <h5>{todo.content}</h5>
-                        <button>Delete</button>
-                        <h6>{todo.creation_date}</h6>
-                    </div>
-                ))}
+                {todos ? 
+                    todos.map(todo => (
+                        <div key={todo.id} style={{display: "flex", justifyContent: "center", alignItems: "center", gap: "16px", width: "fit-content", margin: "auto", backgroundColor: "grey", padding: "16px", borderRadius: "32px"}}>
+                            <input type="checkbox" onChange={(e)=>{handleCheckboxChange(e, todo)}} checked={todo.checked} style={{display: "flex", justifyContent: "center", alignItems: "center", width: "fit-content", margin: "0px", boxSizing: ""}}/>
+                            
+                            {idEditTodo != todo.id ?
+                                <>
+                                    <h5 style={{textDecoration: todo.checked ? "line-through" : "none"}}>{todo.content}</h5>
+                                    <button onClick={()=>{setIdEditTodo(todo.id); setContentEditTodo(todo.content)}}>Edit</button>
+                                </>
+                            :
+                                <>
+                                    <input type="text" onChange={(e)=>{setContentEditTodo(e.target.value)}} value={contentEditTodo}/>
+                                    <button onClick={()=>{handleContentChange(todo)}}>Confirm</button>
+                                </>
+                            }
+
+                            <button onClick={()=>{handleDelete(todo.id)}}>Delete</button>
+                            <h6>{todo.creation_date}</h6>
+                        </div>
+                    ))
+                :
+                    <h2>Vous n'avez pas encore de Todo</h2>
+                }
             </div>
 
         </>
